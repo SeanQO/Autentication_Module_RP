88034eda2c4a542ac7610370c4b5890d7bd36702
SeanQO <seanquintero3@gmail.com>
2021-10-01 21:34:38 -0500

    loggin fully functional

--- Autentication_Module_RP/Pages/Index.cshtml.cs
+++ Autentication_Module_RP/Pages/Index.cshtml.cs
@@ -1,35 +1,57 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Autentication_Module_RP.Data;
+using Autentication_Module_RP.Models;
 
 namespace Autentication_Module_RP.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly Autentication_Module_RP.Data.RDPCUserContext _context;
+
+        public IndexModel(Autentication_Module_RP.Data.RDPCUserContext context)
+        {
+            _context = context;
+        }
+
+        public new IList<User> User { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            User = await _context.User.ToListAsync();
+            
+        }
+
         [BindProperty]
         public string Username { get; set; }
 
         [BindProperty]
         public string Password { get; set; }
 
-        public string Msg { get; set; }
-
-        public void OnGet()
-        {
-        }
-
         public IActionResult OnPost()
         {
-            if (Username.Equals("s") && Password.Equals("s"))
-            {
-                HttpContext.Session.SetString("username", Username);
-                return RedirectToPage("/users/index");
+            if (Username != null && Password != null) {
+                foreach (var user in _context.User)
+                {
+                    if (Username.Equals(user.Username) && Password.Equals(user.Password))
+                    {
+                        HttpContext.Session.SetString("username", Username);
+                        return RedirectToPage("/users/index");
+
+                    }
+
+                }
+
             }
-            else
-            {
-                Msg = "Invalid";
-                return Page();
-            }
+            return Page();
+
         }
+
     }
 }
